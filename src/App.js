b143ecbf83d644751b998d6a8591cd9983ac9bce
haipm99 <haipmse130332@fpt.edu.vn>
2019-09-18 19:58:48 +0700

    add create,delete post , have bug when update post

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Content from './component/AdminArea';
 function App() {
   return (
     <div className="App">
-      <div class="row">
+      <div className="row">
         <Sidebar />
         <Content />
       </div>
--- src/component/AdminArea.jsx
+++ src/component/AdminArea.jsx
@@ -3,14 +3,28 @@ import React, { Component } from 'react';
 //import axios
 
 import axios from 'axios';
+//import jwt-decode
+import jwt_decode from 'jwt-decode';
+//sweet alert
+import Swal from 'sweetalert2';
 class AdminArea extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: []
+            posts: [],
+            title: "",
+            content: "",
+            img: "",
+            post: {},
+            testing: "1",
         }
     }
 
+    onchange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     GetPostFromDB = () => {
         const config = {
             method: 'get',
@@ -18,12 +32,97 @@ class AdminArea extends Component {
         }
 
         return axios(config).then(res => {
-            console.log(res.data);
             this.setState({
                 posts: res.data
             });
         })
     }
+
+    DeletePost = (id) => {
+        var config = {
+            method: "delete",
+            url: `https://localhost:44372/api/Posts/${id}`,
+        }
+
+        return axios(config).then(res => {
+            if (res.status === 200 && res.data != null) {
+                Swal.fire('Create success', '', 'success').then(() => {
+                    this.GetPostFromDB();
+                })
+
+            }
+        })
+    }
+
+    CreatePost = (e) => {
+        e.preventDefault();
+        if (this.state.title !== "" && this.state.content !== "" && this.state.img !== "") {
+            var data = {
+                "title": this.state.title,
+                "content": this.state.content,
+                "img": this.state.img,
+                "userId": jwt_decode(localStorage.getItem("token")).unique_name
+            };
+            var config = {
+                method: 'post',
+                url: 'https://localhost:44372/api/Posts/Create',
+                data: data
+            }
+            return axios(config).then(res => {
+                if (res.status === 200 && res.data !== null) {
+                    Swal.fire('Create success', '', 'success').then(() => {
+                        window.location.reload();
+                    });
+                }
+            })
+        }
+    }
+
+    GetDetail = (id, index) => {
+
+        this.state.posts.forEach((element) => {
+            if (element.id === id) {
+                return this.getPoduct(element, () => { });
+            }
+        });
+    }
+
+    getPoduct = (post, callback) => {
+        this.setState({
+            post: post
+        });
+        callback();
+    }
+
+    UpdatePost = async (e,id) => {
+        
+        e.preventDefault();
+        var data = {
+            "title": this.state.title,
+            "content": this.state.post.content,
+            "img": this.state.post.img,
+            "userId": jwt_decode(localStorage.getItem("token")).unique_name,
+        }
+
+        var config = {
+            method: "patch",
+            url: `https://localhost:44372/api/Posts/${id}`,
+            data: data
+        };
+
+        await axios(config).then(res => {
+            if(res.status === 200 && res.data != null){
+                console.log(res);
+                Swal.fire('Create success', '', 'success').then( () => {
+
+                    this.GetPostFromDB();
+                   // window.locattion.reload();
+                })
+            }
+        })
+
+    }
+
     componentDidMount = () => {
         this.GetPostFromDB();
     }
@@ -35,14 +134,13 @@ class AdminArea extends Component {
                     <td>{item.title}</td>
                     <td>{item.postedDate}</td>
                     <td>{item.img}</td>
-                    <td><a href="/" className="btn btn-success">Update</a></td>
-                    <td><a href="/" className="btn btn-warning">Delete</a></td>
-                    <td><a href="/" className="btn btn-primary">Detail</a></td>
+                    <td><button className="btn btn-warning w-100" onClick={this.DeletePost.bind(this, item.id)}>Delete</button></td>
+                    <td><button data-toggle="modal" onClick={this.GetDetail.bind(this, item.id, index)} className="btn btn-primary w-100" data-target="#myModal2">Detail</button></td>
                 </tr>
             )
         }) : null;
         return (
-            <div style={{ left: "300px", position: "absolute", top: "15%" }}>
+            <div style={{ left: "300px", position: "absolute", top: "5%" }}>
                 <div className="container" style={{ margin: "none" }}>
                     <div className="card text-center" style={{ width: "1100px" }}>
                         {/* Header */}
@@ -75,7 +173,6 @@ class AdminArea extends Component {
                                         <th>Title</th>
                                         <th>Posted Date</th>
                                         <th>Img</th>
-                                        <th>Edit</th>
                                         <th>Delete</th>
                                         <th>Detail</th>
                                     </tr>
@@ -88,17 +185,18 @@ class AdminArea extends Component {
                         {/* Footer */}
                         <div className="card-footer myCardFooter">
                             <nav>
-                                <ul className="pagination justify-content-center" id="ulPhanTrang">
+                                <ul className="pagination justify-content-center">
                                 </ul>
                             </nav>
                         </div>
                     </div>
                 </div>
+                {/* modal */}
                 <div className="modal fade" id="myModal">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <header className="head-form mb-0">
-                                <h2 id="header-title">Log In</h2>
+                                <h2 id="header-title">Create Posts</h2>
                             </header>
                             {/* Modal Header */}
                             {/* 	<div class="modal-header">
@@ -106,75 +204,59 @@ class AdminArea extends Component {
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 				</div> */}
                             {/* Modal body */}
-                            <div className="modal-body">
-                                <form>
+                            <form onSubmit={this.CreatePost}>
+                                <div className="modal-body">
                                     <div className="form-group">
                                         <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-user" /></span>
-                                            </div>
-                                            <input type="msnv" name="msnv" id="msnv" className="form-control input-sm" placeholder="Mã số nhân viên" />
+                                            <input name="title" className="form-control input-sm" placeholder="Title" onChange={this.onchange} />
                                         </div>
-                                        <span className="sp-thongbao" id="tbMaNV" />
                                     </div>
                                     <div className="form-group">
                                         <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-address-book" /></span>
-                                            </div>
-                                            <input type="name" name="name" id="name" className="form-control input-sm" placeholder="Họ và tên" />
+                                            <textarea className="form-control" name="content" placeholder="Content" rows="3" onChange={this.onchange}></textarea>
                                         </div>
-                                        <span className="sp-thongbao" id="tbTen" />
                                     </div>
                                     <div className="form-group">
                                         <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-envelope" /></span>
-                                            </div>
-                                            <input type="email" name="email" id="email" className="form-control input-sm" placeholder="Email" />
+                                            <input type="file" name="img" className="form-control" placeholder="Image" onChange={this.onchange} />
                                         </div>
-                                        <span className="sp-thongbao" id="tbEmail" />
+                                    </div>
+                                </div>
+                                {/* Modal footer */}
+                                <div className="modal-footer" id="modal-footer">
+                                    <button type="submit" className="btn btn-success">Add Post</button>
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                {/* model 2 */}
+                <div className="modal fade" id="myModal2">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <header className="head-form mb-0">
+                                <h2 id="header-title">Post Detail</h2>
+                            </header>
+                            <form onSubmit= {this.UpdatePost.bind(this,this.state.post.id)}>
+                                <div className="modal-body">
+                                    <div className="form-group">
+                                        <div className="input-group">
+                                            <input type="text" name="title" className="form-control input-sm" defaultValue={this.state.post.title} onChange={this.onchange} />
+                                        </div>
                                     </div>
                                     <div className="form-group">
                                         <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-key" /></span>
-                                            </div>
-                                            <input type="password" name="password" id="password" className="form-control input-sm" placeholder="Mật khẩu" />
+                                            <textarea className="form-control" name="content" rows="3" value={this.state.post.content} />
                                         </div>
-                                        <span className="sp-thongbao" id="tbMatKhau" />
                                     </div>
-                                    <div className="form-group">
-                                        <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-calendar" /></span>
-                                            </div>
-                                            <input type="text" name="ngaylam" id="datepicker" className="form-control" placeholder="Ngày làm" />
-                                        </div>
-                                        <span className="sp-thongbao" id="tbNgay" />
-                                    </div>
-                                    <div className="form-group">
-                                        <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text"><i className="fa fa-briefcase" /></span>
-                                            </div>
-                                            <select className="form-control" id="chucvu">
-                                                <option>Chọn chức vụ</option>
-                                                <option>Sếp</option>
-                                                <option>Trưởng phòng</option>
-                                                <option>Nhân viên</option>
-                                            </select>
-                                        </div>
-                                        <span className="sp-thongbao" id="tbChucVu" />
-                                    </div>
-                                </form>
-                            </div>
-                            {/* Modal footer */}
-                            <div className="modal-footer" id="modal-footer">
-                                <button id="btnThemNV" type="button" className="btn btn-success">Thêm người dùng</button>
-                                <button id="btnCapNhatNV" type="button" className="btn btn-success">Cập nhật</button>
-                                <button id="btnDong" type="button" className="btn btn-danger" data-dismiss="modal">Đóng</button>
-                            </div>
+                                </div>
+                                {/* Modal footer */}
+                                <div className="modal-footer" id="modal-footer">
+                                    <button type="submit" className="btn btn-success">Update</button>
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
