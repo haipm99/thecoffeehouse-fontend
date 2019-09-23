7fa4f29784d544a8cac6218351d8d5dea2871eb1
haipm99 <haipmse130332@fpt.edu.vn>
2019-09-23 12:22:29 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //component
+import Login from './Page/LoginPage/Login';
 import ManagePost from './Page/ManagePost';
 import ManageProduct from './Page/ManageProduct';
 import MainPage from './Page/MainPage/MainPage';
-import Login from './Page/LoginPage/Login';
 function App() {
   return (
     <div className="App">
       <Router>
           <Switch>
+            <Route path="/login" exact component = {Login} />
             <Route path="/" exact component = {MainPage} />
             <Route path="/managePost" exact component={ManagePost} />
             <Route path="/manageProducts" exact component={ManageProduct} />
-            <Route path="/login" exact component = {Login} />
           </Switch>
         </Router>
     </div>
--- src/Page/LoginPage/Login.jsx
+++ src/Page/LoginPage/Login.jsx
@@ -2,12 +2,72 @@ import React, { Component } from 'react';
 
 //import css
 import './login.css';
+
+//import axios
+import axios from 'axios';
+//jwtdecode
+import jwt from 'jwt-decode';
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username :"",
+            password:"",
+        }
+    }
+    
+    
+    Login = (e) =>{
+        e.preventDefault();
+        var config = {
+            method:"post",
+            url:"https://localhost:44372/api/AppUser/login",
+            data : {
+                "username" :this.state.username,
+                "password":this.state.password,
+            }
+        };
+
+        return  axios(config).then(res => {
+            if(res.status === 200 && res.data !== null){
+                localStorage.setItem("token",res.data.access_token);
+                if(jwt(res.data.access_token).role === "admin"){
+                    window.location.href = "/ManagePost";
+                }
+                else{
+                    window.location.href = "/";
+                }
+            }
+        }) 
+    }
+
+    onchange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render() {
         return (
-            <div className="login-container" style={{ width: "100vw", height: "100vh", paddingTop:"250px" }}>
+            <div className="login-container" style={{ width: "100vw", height: "100vh", paddingTop: "200px" }}>
                 <div>
-                    
+                    <form method="POST" style={{backgroundColor:" rgba(0, 0, 0, 0.5) "}} onSubmit = {this.Login}>
+                        <center><h1 style={{color:"white"}}>Login</h1></center>
+                        <div className="form-group">
+                            <div className="input-group">
+                                <input name="username" className="form-control input-sm" placeholder="Username" onChange={this.onchange} />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="input-group">
+                                <input className="form-control" type="password" name="password" placeholder="Password" onChange={this.onchange} />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="input-group">
+                                <input className="btn btn-success" type="submit" value="Login"/>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
